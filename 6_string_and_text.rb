10efe33a71881c1d5ad46d8a1e632f24248bbc8b
Sasho1989 <caki.jova@gmail.com>
2020-02-15 01:35:29 +0100

    Adding two files

---
+++ 6_string_and_text.rb
@@ -0,0 +1,42 @@
+# sets 3 types of people
+types_of_people = 3
+
+# varible x within a string defining types of people
+x = "There are #{types_of_people} types of people."
+
+# set variable math to math
+math = "math"
+
+# set variable do_not to another kind of people
+do_not = "don't"
+
+# sets variable y that interpolates string putting the values of variable math and do_not togheter
+y = "Those who know #{math} and those who #{do_not}."
+
+# print out x
+puts x
+
+# print out y
+puts y
+
+# print out x variable in string interpolation
+puts "I said: #{x}."
+
+# print out y in a string
+puts "I also said: '#{y}'"
+
+# define var hilarous to false
+hilarious = false
+
+#set var joke_evaluation to string containing variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print out joke_evaluation
+puts joke_evaluation
+
+# sets variable w and e to strings
+w = "This is left side of..."
+e = "a string with a right side."
+
+# print out concatenation of w and e into a string
+puts w + e
---
+++ 7_print_print_print.rb
@@ -0,0 +1,32 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+I can go on and on until i use those double quotes again i guess.
+"""
+
+
+
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\n ona a line."
+space_cat = "\s I'm a space cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\nt* Grass
+"""
+puts tabby_cat
+puts persian_cat
+puts space_cat
+puts fat_cat
